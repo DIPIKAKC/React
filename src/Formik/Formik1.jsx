69bd45c88ac1e65cb16ec6c79ec297f4a1535843
Dipika K.C <dipikak0323@gmail.com>
2025-09-25 23:41:21 +0545

    react formik

---
+++ src/Formik/Formik1.jsx
@@ -0,0 +1,51 @@
+//npm i formik
+//npm i uuid-=>for unique id
+
+import { v4 as uuidv4 } from 'uuid';
+export default function Formik1(){
+  const [data, setData] = useState([]);
+
+const handleDelete = (id) => {
+  setData((prev) => prev.filter((user) => user.id !== id)); //pathako user ra del user same bhayo bhane
+};
+  return(
+    <div className="p-5">
+      <Formik
+        initialValues={{username:''}}
+        onSubmit={(val,{resetForm})=>{
+          console.log(val);
+          setData((prev)=>[...prev,{
+            username:val.username,
+            id: uuidv4()
+          }])
+          resetForm(); //submit garesi reset gardincha
+        }}
+      >
+        {({handleChange, handleSubmit, values})=>{
+          return <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <Input 
+                variant='bordered'
+                // onChange={(e)=>e.target.value} 
+                onChange={handleChange} //change hune bitikei yo line le handle change bhanne fxn lai call garcha
+                label='username' 
+                name="username" 
+                value={values.username}
+                />
+            </div>
+            <Button type="submit">Submit</Button>
+          </form>
+        }}
+      </Formik>
+
+
+      {data.map((user, i)=>{
+        return <div key={i}>
+          <h1>{user.username}</h1>
+          <Button onPress={()=>{handleDelete(user.id)}}>Delete</Button>
+        </div>
+      })}
+
+    </div>
+  )
+}
--- src/Hooks/UseRef.jsx
+++ src/Hooks/UseRef.jsx
@@ -6,18 +6,45 @@ import { Button, Input } from "@heroui/react";
 import { useRef } from "react";
 
 export default function UseRef() {
-  const some = useRef();
-  const handleRef = () => {
-    some.current.focus();
+    const some = useRef();
+    const handleRef = () => {
+        some.current.focus();
+    }
+    const [data, setData] = useState([]);
 
-  }
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-  return (
-    <div className="p-5 space-y-5">
-      <div>
-        <Input ref={some} label="Label" />
-      </div>
-      <Button onClick={handleRef}>Button</Button>
-    </div>
-  )
+    }
+
+    return (
+
+        <div className="p-5 space-y-5">
+            <div>
+                <Input ref={some} label="Label" />
+            </div>
+            <Button onClick={handleRef}>Button</Button>
+        </div>
+
+        // <div>
+
+        // <div className="p-5 max-w-[300px]">
+
+
+        //   <form onSubmit={handleSubmit} className="space-y-4">
+
+        //     <Input
+        //       onChange={handleSubmit}
+        //       label='email' name="email" />
+
+        //     <Button type="submit">Submit</Button>
+        //   </form>
+
+        // </div>
+
+    )
 }
+
+
+
+
